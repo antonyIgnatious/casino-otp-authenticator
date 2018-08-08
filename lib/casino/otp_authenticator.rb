74f35cb5437d1bbfab3d4e8faf74bd67578928d7
Antony Ignatious <antony.ignatious@neogrowth.in>
2018-08-08 23:54:05 +0530

    added: initialise for authenticator

--- lib/casino/otp_authenticator.rb
+++ lib/casino/otp_authenticator.rb
@@ -6,9 +6,19 @@ class CASino::OtpAuthenticator
     self.abstract_class = true
   end
 
-  # @param [Hash] options
   def initialize(options)
-
+    option_initialize(options)
+    validate_option_data
+    user_model_name = classify_table(@options[:user_table])
+    otp_model_name = classify_table(@options[:otp_table])
+    user_model_class_name = "#{self.class.to_s}::#{user_model_name}"
+    otp_model_class_name = "#{self.class.to_s}::#{otp_model_name}"
+    load_class(user_model_class_name, @options[:user_table])
+    load_class(otp_model_class_name, @options[:otp_table])
+    @user_model = user_model_class_name.constantize
+    @otp_model = otp_model_class_name.constantize
+    @user_model.establish_connection @options[:connection]
+    @otp_model.establish_connection @options[:connection]
   end
 
   def validate(username, password)
@@ -21,5 +31,35 @@ class CASino::OtpAuthenticator
 
   rescue ActiveRecord::RecordNotFound
     nil
+  end
+
+  private
+
+  def option_initialize(options)
+    unless options.respond_to?(:deep_symbolize_keys)
+      raise ArgumentError, 'When assigning attributes, you must pass a hash as an argument.'
+    end
+    @options = options.deep_symbolize_keys
+  end
+
+  def validate_option_data
+    raise ArgumentError, 'User Table name is missing' unless @options[:user_table]
+    raise ArgumentError, 'OTP Table name is missing' unless @options[:otp_table]
+  end
+
+  def classify_table(table_name)
+    if @options[:connection].kind_of?(Hash) && @options[:connection][:database]
+      model_name = "#{@options[:connection][:database].gsub(/[^a-zA-Z]+/, '')}_#{table_name}"
+    end
+    model_name.classify
+  end
+
+  def load_class(model_class_name, table_name)
+    eval <<-END
+        class #{model_class_name} < AuthDatabase
+          self.table_name = "#{table_name}"
+          self.inheritance_column = :_type_disabled
+        end
+      END
   end
 end
